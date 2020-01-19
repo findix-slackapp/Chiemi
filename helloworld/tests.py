1023c305b0e5ae4126188215194a368addde9102
Takako Kikkawa <takako@tkMacBook.local>
2020-01-19 15:16:42 +0900

    指示に従いボットが発言する #2

--- helloworld/tests.py
+++ helloworld/tests.py
@@ -1,5 +1,8 @@
 from django.test import TestCase, RequestFactory
 from helloworld.views import HomePageView
+from unittest import mock
+import slack
+import slackbot_settings
 
 class HelloWorldTestCase(TestCase):
 
@@ -12,3 +15,64 @@ class HelloWorldTestCase(TestCase):
         self.assertEqual(response.get('content-type'), 'text/html; charset=utf-8')
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'Congratulations')
+
+    def test_post_return_no_user(self):
+        data = {}
+        request = self.factory.post('/', data)
+        response = HomePageView.as_view()(request)
+        self.assertEqual(response.get('content-type'), 'text/plain')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'no user')
+
+    def test_post_return_unauthorized_user(self):
+        data = {"user_id": "123"}
+        request = self.factory.post('/', data)
+        response = HomePageView.as_view()(request)
+        self.assertEqual(response.get('content-type'), 'text/plain')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'unauthorized user')
+
+    def test_post_return_no_parameter(self):
+        data = {"user_id": "UB9AVTDT3"}
+        request = self.factory.post('/', data)
+        response = HomePageView.as_view()(request)
+        self.assertEqual(response.get('content-type'), 'text/plain')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'no parameter')
+
+    def test_post_return_invalid_parameter(self):
+        data = {"user_id": "UB9AVTDT3", "text": "prj-slackapp-test"}
+        request = self.factory.post('/', data)
+        response = HomePageView.as_view()(request)
+        self.assertEqual(response.get('content-type'), 'text/plain')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'invalid parameter')
+
+    def test_post_return_no_message(self):
+        data = {"user_id": "UB9AVTDT3", "text": "prj-slackapp-test,"}
+        request = self.factory.post('/', data)
+        response = HomePageView.as_view()(request)
+        self.assertEqual(response.get('content-type'), 'text/plain')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'no message')
+
+    def test_post_return_invalid_channel(self):
+        data = {"user_id": "UB9AVTDT3", "text": "abc,test"}
+        request = self.factory.post('/', data)
+        response = HomePageView.as_view()(request)
+        self.assertEqual(response.get('content-type'), 'text/plain')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'invalid channel')
+
+    def test_post_call_chat_postMessage(self):
+        client = slack.WebClient(token=slackbot_settings.API_TOKEN)
+        slack.WebClient = mock.MagicMock(return_value=client)
+        client.chat_postMessage = mock.MagicMock()
+
+        data = {"user_id": "UB9AVTDT3", "text": "prj-slackapp-test,test"}
+        request = self.factory.post('/', data)
+        response = HomePageView.as_view()(request)
+        self.assertEqual(response.get('content-type'), 'text/plain')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'test')
+        client.chat_postMessage.assert_called_with(channel="CHXS0FH5M",text="test",as_user=True)
---
+++ slackbot_settings.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+import boto3
+import os
+
+# botアカウントのトークンを指定
+API_TOKEN = os.environ.get('CHIEMI_SLACKAPP_BOT_TOKEN')
+if API_TOKEN is None:
+    ssm = boto3.client('ssm', region_name='ap-northeast-1')
+    parameter = ssm.get_parameter(Name='chiemi_slackapp_bot_token', WithDecryption=True)
+    API_TOKEN = parameter['Parameter']['Value']
+
+# このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
+DEFAULT_REPLY = "Sorry but I didn't understand you"
+
+# プラグインスクリプトを置いてあるサブディレクトリ名のリスト
+PLUGINS = ['plugins']
